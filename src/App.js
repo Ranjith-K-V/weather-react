ed1c6aaef17273788e7b992bda3e0bb8d92786f8
Ranjith <ranjith.k.v@outlook.com>
2023-09-04 06:56:00 +0530

    Design Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=e2a8e0dcb62fb0d4f6ade73cd9ee5520`
+
+
   return (
     <div className="App">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>65°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">65°F</p>
+            <p>Feels like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
       
     </div>
   );
